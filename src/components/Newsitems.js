87c218462966c63647796ac80d9cecefaa6ad76d
SM ORIGINALS <SMOS-Github@users.noreply.github.com>
2025-01-01 22:22:43 +0530

    update
    news app ready.
    here on this runs with external data next update run with APIs data.

--- src/components/Newsitems.js
+++ src/components/Newsitems.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react';
 import Cards from './Cards';
+import source from '../source.json';
+
+
 export default class Newsitems extends Component {
+        constructor(){
+            super();
+            this.state = {
+                articles: source,  //internal json source.
+                loading: false
+            }
+                
+        }
     render() {
+
+
         return (
             <>
                 <div className="container my-4">
                     <div className="row">
-                        <div className="col-md-4 my-2">
-                            <Cards title="Alex" description="The quick brown fox jumps over the lazy Dog." imageUrl="https://cassette.sphdigital.com.sg/image/straitstimes/7211f7c3d112c7af36582e156d55ab47f2d5c0d48b0f68053e88c6f3f17e4f30"/>
-                        </div>
-                        <div className="col-md-4 my-2">
-                            <Cards title="Alex" description="The quick brown fox jumps over the lazy Dog." imageUrl="https://cassette.sphdigital.com.sg/image/straitstimes/7211f7c3d112c7af36582e156d55ab47f2d5c0d48b0f68053e88c6f3f17e4f30"/>
-                        </div>
-                        <div className="col-md-4 my-2">
-                            <Cards title="Alex" description="The quick brown fox jumps over the lazy Dog." imageUrl="https://cassette.sphdigital.com.sg/image/straitstimes/7211f7c3d112c7af36582e156d55ab47f2d5c0d48b0f68053e88c6f3f17e4f30"/>
-                        </div>
-                        <div className="col-md-4 my-2">
-                            <Cards title="Alex" description="The quick brown fox jumps over the lazy Dog." imageUrl="https://cassette.sphdigital.com.sg/image/straitstimes/7211f7c3d112c7af36582e156d55ab47f2d5c0d48b0f68053e88c6f3f17e4f30"/>
-                        </div>
+                        {
+                            this.state.articles.map((e) => {
+
+                                return  <div className="col-md-4 my-2">
+                                    <Cards title={<b>{e.title}</b>} description={e.description} imageUrl={e.urlToImage} newsUrl={e.url} />
+                                </div>
+                            })
+                        }
+
                     </div>
                 </div>
             </>
